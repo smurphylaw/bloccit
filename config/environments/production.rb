47f475155247c6f63b7290b6f5c3422323eb6c1a
smurphylaw <smurphylaw@gmail.com>
2014-05-24 14:42:35 -0400

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # http://smurphylaw-bloccit.herokuapp.com
+  config.action_mailer.default_url options = { host: 'smurphylaw-bloccit.herokuapp.com'}
 end
