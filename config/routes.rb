abfb3629fdb8d0aa7600447523ae1501acedec17
smurphylaw <smurphylaw@gmail.com>
2014-06-08 17:17:38 -0600

    Fixed older bugs

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Bloccit::Application.routes.draw do
   devise_for :users
   
   resources :users, only: [:update]
-  
-  resources :posts
 
   resources :topics do
     resources :posts, except: [:index]
@@ -13,4 +11,5 @@ Bloccit::Application.routes.draw do
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
+  
 end
