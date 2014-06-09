d96707cc4b72b0bfab5745f3e7543c357fa8dfc3
smurphylaw <smurphylaw@gmail.com>
2014-06-09 14:34:32 -0600

    Installed Carrierwave

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def update
     if current_user.update_attributes(user_params)
       flash[:notice] = "User information updated"
-      redirect_to_edit_user_registration_path(current_user)
+      redirect_to root_path
     else
-      render "devise/registration/edit"
+      render "devise/registrations/edit"
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :avatar)
+    params.require(:user).permit(:name, :avatar, :avatar_cache)
   end
 end
