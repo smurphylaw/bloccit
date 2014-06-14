73484e9bba2f0b08e24bf1b116ef660da4eb92cc
smurphylaw <smurphylaw@gmail.com>
2014-06-14 00:56:54 -0600

    Implemented favoriting

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
   after_create :create_vote
+  has_many  :favorites, dependent: :destroy
   mount_uploader :image, ImageUploader
 
   default_scope { order('rank DESC') } 
